3ec190d23307673e99c2f610af83c56800ddab63
21721405 <164751580+21721405@users.noreply.github.com>
2025-04-09 00:11:01 +0400

    Updated the flatland-3.html and flatland-script.js
    Added new codes for this which adds a hover effect on the button in the colors red, green and grey,

---
+++ js/flatland-script.js
@@ -0,0 +1,17 @@
+// Step 3: Multiple Events - makeRed, makeGreen, makeGray
+
+function makeRed() {
+    const square = document.getElementById("square");
+    if (square) square.style.backgroundColor = "red";
+  }
+  
+  function makeGreen() {
+    const square = document.getElementById("square");
+    if (square) square.style.backgroundColor = "green";
+  }
+  
+  function makeGray() {
+    const square = document.getElementById("square");
+    if (square) square.style.backgroundColor = "gray";
+  }
+  
